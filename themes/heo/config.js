4d097dca92500fb6796644315d6cc3433dbf74da
wanglizong <1580789234@qq.com>
2024-11-22 00:24:05 +0800

    update

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -115,10 +115,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: 'Communication Channel',
+  HEO_SOCIAL_CARD_TITLE_2: 'Join our community to discuss and share',
+  HEO_SOCIAL_CARD_TITLE_3: 'Click to join the community',
+  HEO_SOCIAL_CARD_URL: 'https://www.hotspotdaily.com/',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
